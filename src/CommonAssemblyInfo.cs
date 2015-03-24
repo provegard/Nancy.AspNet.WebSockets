63830d73f147c9cb27fa612919b5ee640890eb3c
Per Rovegård <per@rovegard.se>
2015-03-24 12:29:10 +0100

    Bump version to 0.0.2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy.AspNet.WebSockets")]
 [assembly: AssemblyProduct("Nancy.AspNet.WebSockets")]
 [assembly: AssemblyCopyright("Copyright (C) 2015 Per Rovegard")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
 
